a894b213777d906ec46dee862aa4758b5ec187c7
edwardm8 <edwardm825@gmail.com>
2023-12-04 23:46:09 -0700

    added gltf of duck

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,110 @@
 <script>
 	import { browser } from '$app/environment'
 	import * as THREE from "three"
-
+	// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+	
 	if (browser){
+		const canvas = document.querySelector('canvas.webgl')
+		console.log(canvas)
 		const scene = new THREE.Scene();
-		const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-		const renderer = new THREE.WebGLRenderer();
-		renderer.setSize( window.innerWidth, window.innerHeight );
-		document.body.appendChild( renderer.domElement );
-
-		const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-		const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-		const cube = new THREE.Mesh( geometry, material );
-		scene.add( cube );
-
-		camera.position.z = 5;
-
-		function animate() {
-			requestAnimationFrame( animate );
-
-			cube.rotation.x += 0.01;
-			cube.rotation.y += 0.01;
-
-			renderer.render( scene, camera );
+		const sizes = {
+			width: window.innerWidth,
+			height: window.innerHeight
 		}
 
-		animate();
+		window.addEventListener('resize', () =>
+		{
+			// Update sizes
+			sizes.width = window.innerWidth
+			sizes.height = window.innerHeight
+
+			// Update camera
+			camera.aspect = sizes.width / sizes.height
+			camera.updateProjectionMatrix()
+
+			// Update renderer
+			renderer.setSize(sizes.width, sizes.height)
+			renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+		})
+
+		const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+		camera.position.set(2, 2, 2)
+		scene.add(camera)
+
+		// Controls
+		const controls = new OrbitControls(camera, canvas)
+		controls.target.set(0, 0.75, 0)
+		controls.enableDamping = true
+
+		// const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		const gltfLoader = new GLTFLoader()
+
+		const renderer = new THREE.WebGLRenderer({
+    	canvas: canvas
+		})
+		renderer.shadowMap.enabled = true
+		renderer.shadowMap.type = THREE.PCFSoftShadowMap
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+		document.body.appendChild( renderer.domElement );
+
+		const ambientLight = new THREE.AmbientLight(0xffffff, 2.4)
+		scene.add(ambientLight)
+
+		const directionalLight = new THREE.DirectionalLight(0xffffff, 1.8)
+		directionalLight.castShadow = true
+		directionalLight.shadow.mapSize.set(1024, 1024)
+		directionalLight.shadow.camera.far = 15
+		directionalLight.shadow.camera.left = - 7
+		directionalLight.shadow.camera.top = 7
+		directionalLight.shadow.camera.right = 7
+		directionalLight.shadow.camera.bottom = - 7
+		directionalLight.position.set(5, 5, 5)
+		scene.add(directionalLight)
+
+		gltfLoader.load(
+    '../assets/3d/Duck/glTF/Duck.gltf',
+    (gltf) =>
+    {
+        // console.log('success')
+        // console.log(gltf)
+				scene.add(gltf.scene.children[0])
+    },
+    (progress) =>
+    {
+        // console.log('progress')
+        // console.log(progress)
+    },
+    (error) =>
+    {
+        // console.log('error')
+        // console.log(error)
+    }
+	)
+
+			const clock = new THREE.Clock()
+		let previousTime = 0
+
+		const tick = () =>
+		{
+				const elapsedTime = clock.getElapsedTime()
+				const deltaTime = elapsedTime - previousTime
+				previousTime = elapsedTime
+
+				// Update controls
+				controls.update()
+
+				// Render
+				renderer.render(scene, camera)
+
+				// Call tick again on the next frame
+				window.requestAnimationFrame(tick)
+		}
+
+tick()
 	}
 </script>
 
